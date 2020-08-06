c976760ddc5db08075f50e7c47eacf973f6a0c43
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2020-08-06 20:23:13 +0200

    export data types

--- src/ProcessMining.jl
+++ src/ProcessMining.jl
@@ -1,6 +1,13 @@
 module ProcessMining
+
     import EzXML
+
+    export Event
+    export Trace
+    export EventLog
+    export read_xes
+
     include("types.jl")
     include("io.jl")
-    export read_xes
+
 end
