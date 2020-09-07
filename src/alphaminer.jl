7320d84b79edb668f0f1cc38e8084d29ed80d6ad
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2020-09-07 20:14:52 +0200

    some attempts for alpha step 4

--- src/alphaminer.jl
+++ src/alphaminer.jl
@@ -16,22 +16,35 @@
 # 7. Determine the flow relation
 # 8. Put everything together
 
-using ProcessMining
-
-eventlog = read_xes(joinpath("data", "Performance.xes"))
-
-mutable struct Footprint{T}
-    colnames::Array{T}
-    rownames::Array{T}
-    relation::Matrix{Symbol}
+# OBJECTS FOR WORKFLOW
+begin
+    using ProcessMining
+    eventlog = read_xes(joinpath("data", "Performance.xes"))
+    eventlog2 = read_xes(joinpath("data", "road_fines.xes"))
 end
+# ------------------------
 
+# MAYBE NOT NEEDED
+# mutable struct Footprint{T}
+#     colnames::Array{T}
+#     rownames::Array{T}
+#     relation::Matrix{Symbol}
+# end
+# ------------------------
+
+# 1) GET RAW TRACES AND ACTIVITY SET FROM THE LOG
 function extract_event_traces(eventlog::EventLog)
     return [
         [event.name for event in trace.events] for trace in eventlog.traces
     ]
 end
 
+function extract_activity_set(extracted_event_traces::AbstractArray)
+    return unique(collect(Iterators.flatten(extracted_event_traces)))
+end
+# ------------------------
+
+# 2 + 3) SIMPLE STEPS: START AND END ACTIVITIES
 function extract_start_activities(extracted_event_traces::AbstractArray)
     return unique([first(trace) for trace in extracted_event_traces])
 end
@@ -39,13 +52,11 @@ end
 function extract_end_activities(extracted_event_traces::AbstractArray)
     return unique([last(trace) for trace in extracted_event_traces])
 end
+# ------------------------
 
-function extract_activity_set(extracted_event_traces::AbstractArray)
-    return unique(collect(Iterators.flatten(extracted_event_traces)))
-end
-
+# 4a) EXTRACT RELATIONS FROM THE ACTIVITY LOG
 function extract_direct_succession_relation(extracted_event_traces)
-    direct_succession_relation = Set([])
+    direct_succession_relation = Set(Tuple{String, String}[])
     for trace in extracted_event_traces
         for i in 1:length(trace)
             if !(i == length(trace))
@@ -57,7 +68,7 @@ function extract_direct_succession_relation(extracted_event_traces)
 end
 
 function extract_causality_relation(direct_succession_relation)
-    causality_relation = Set([])
+    causality_relation = Set(Tuple{String, String}[])
     for tup in direct_succession_relation
         if !((tup[2], tup[1]) in direct_succession_relation)
             push!(causality_relation, tup)
@@ -67,7 +78,7 @@ function extract_causality_relation(direct_succession_relation)
 end
 
 function extract_parallel_relation(direct_succession_relation)
-    parallel_relation = Set([])
+    parallel_relation = Set(Tuple{String, String}[])
     for tup in direct_succession_relation
         if ((tup[2], tup[1]) in direct_succession_relation) && !((tup[2], tup[1]) in parallel_relation)
             push!(parallel_relation, tup)
@@ -81,13 +92,14 @@ function extract_choice_relation(direct_succession_relation, activity_set)
     choice_relation = setdiff(all_pairs, direct_succession_relation)
     return choice_relation
 end
+# ------------------------
 
+# 4b) GET SET X_L
 function extract_place_pairs(causality_relation, choice_relation)
     place_pairs = []
     for mapping in causality_relation
         push!(place_pairs, (Set([mapping[1]]), Set([mapping[2]])))
     end
-    print(place_pairs)
     place_pairs = merge_append_sets!(place_pairs, choice_relation)
     return place_pairs
 end
@@ -111,36 +123,54 @@ function merge_append_sets!(place_pairs, choice_relation)
     return place_pairs
 end
 
+# !!!!KNACKPUNKT!!!!
 function is_unrelated(p1, p2, choice_relation)
     is_unrelated_predecessor = (
         (issubset(p1[1], p2[1]) || issubset(p2[1], p1[1]))
-        && ((p1[1], p2[1]) in choice_relation || ((p2[1], p1[1]) in choice_relation))
+        && (((first(p1[1]), first(p2[1])) in choice_relation) || ((first(p2[1]), first(p1[1])) in choice_relation))
     )
     is_unrelated_successor = (
         (issubset(p1[2], p2[2]) || issubset(p2[2], p1[2]))
-        && (((p1[2], p2[2]) in choice_relation) || (p2[2], p1[2]) in choice_relation)
+        && (((first(p1[2]), first(p2[2])) in choice_relation) || ((first(p2[2]), first(p1[2])) in choice_relation))
     )
     return is_unrelated_predecessor && is_unrelated_successor
 end
+# ------------------------
 
+
+# --------------------------- TESTING --------------------------- #
 
 pp = extract_place_pairs(causality_relation, choice_relation)
 
-("A_ACTIVATED", "A_ACCEPTED") in choice_relation
+merge_append_sets!(pp, choice_relation)
 
-a = Set([1, 2, 3])
+is_unrelated(pp[14], pp[16], choice_relation)
 
-a = push!(a, 5)
+
 
 Matrix{Symbol}(undef, 100, 100)
 
-extracted_event_traces = extract_event_traces(eventlog)
 
-start_activities = extract_start_activities(extracted_event_traces)
-end_activities = extract_end_activities(extracted_event_traces)
-activity_set = extract_activity_set(extracted_event_traces)
+# TESTING
 
-direct_succession_relation = extract_direct_succession_relation(extracted_event_traces)
-causality_relation = extract_causality_relation(direct_succession_relation)
-parallel_relation = extract_parallel_relation(direct_succession_relation)
-choice_relation = extract_choice_relation(direct_succession_relation, activity_set)
+begin
+    extracted_event_traces = extract_event_traces(eventlog)
+    start_activities = extract_start_activities(extracted_event_traces)
+    end_activities = extract_end_activities(extracted_event_traces)
+    activity_set = extract_activity_set(extracted_event_traces)
+    direct_succession_relation = extract_direct_succession_relation(extracted_event_traces)
+    causality_relation = extract_causality_relation(direct_succession_relation)
+    parallel_relation = extract_parallel_relation(direct_succession_relation)
+    choice_relation = extract_choice_relation(direct_succession_relation, activity_set)
+end
+
+begin
+    extracted_event_traces = extract_event_traces(eventlog2)
+    start_activities = extract_start_activities(extracted_event_traces)
+    end_activities = extract_end_activities(extracted_event_traces)
+    activity_set = extract_activity_set(extracted_event_traces)
+    direct_succession_relation = extract_direct_succession_relation(extracted_event_traces)
+    causality_relation = extract_causality_relation(direct_succession_relation)
+    parallel_relation = extract_parallel_relation(direct_succession_relation)
+    choice_relation = extract_choice_relation(direct_succession_relation, activity_set)
+end
