61744921c0ba33bd8fb4c48e87fc30c51bab718d
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-03-07 15:13:09 +0100

    Refactored petrinets module
    * next step maybe literal submodule

--- src/ProcessMining.jl
+++ src/ProcessMining.jl
@@ -2,10 +2,10 @@ module ProcessMining
 
     import EzXML
     import DataFrames
-    using AutoHashEquals
-    using Pipe
+    # using AutoHashEquals
 
     const DEFAULT_NAMESPACE = "http://www.xes-standard.org/"
+
 
     export Event
     export Trace
@@ -13,6 +13,17 @@ module ProcessMining
     export read_xes
     export info
     export length
+    export SimplePetriNet
+    export WorkflowNet
+    export Transition
+    export Place
+    export fire!
+    export fire
+    export fire_sequence
+    export is_enabled
+    export add_place!
+    export add_transition!
+
 
     include("types.jl")
     include("petrinets.jl")
--- src/petrinets.jl
+++ src/petrinets.jl
@@ -1,92 +1,105 @@
-@auto_hash_equals mutable struct Transition
-    id::Int
-end
+abstract type AbstractPetriNet end
+
 
 mutable struct Place
     id::Int
     marking::Int
 end
 
-mutable struct PetriNet
+
+mutable struct Transition
+    id::Int
+    from::Array{Place}
+    to::Array{Place}
+end
+# TODO: do we need auto_hash_equals? -> @auto_hash_equals
+#       decide later
+
+
+Base.@kwdef mutable struct SimplePetriNet <: AbstractPetriNet
     places::Array{Place}
     transitions::Array{Transition}
-    arcs::Array{Union{Tuple{Place, Transition}, Tuple{Transition, Place}}}
 end
 
-function fire!(transition::Transition, model::PetriNet)
-    if is_enabled(transition, model)
-        update_markings!(transition, model)
+
+Base.@kwdef mutable struct WorkflowNet <: AbstractPetriNet
+    source::Int
+    sink::Int
+    places::Array{Place}
+    transitions::Array{Transition}
+end
+
+
+function fire!(model::AbstractPetriNet, transition_id::Int)
+    if is_enabled(model, transition_id)
+        for f in model.transitions[transition_id].from
+            f.marking -= 1
+        end
+        for t in model.transitions[transition_id].to
+            t.marking += 1
+        end
     else
-        print("Transition is not enabled\nDid not fire")
+        println("Transition is not enabled.")
     end
     return model
 end
 
-function is_enabled(transition::Transition, model::PetriNet)
-    predecessors = get_predecessors(transition, model)
-    return sum([place.marking > 0 for place in predecessors]) == length(predecessors)
+
+function fire(model::AbstractPetriNet, transition_id::Int)
+    updated_model = deepcopy(model)
+    fire!(updated_model, transition_id)
+    return updated_model
 end
 
-function get_predecessors(transition::Transition, model::PetriNet)
-    return [arc[1] for arc in model.arcs if arc[2] == transition]
-end
 
-function get_successors(transition::Transition, model::PetriNet)
-    return [arc[2] for arc in model.arcs if arc[1] == transition]
-end
-
-function update_markings!(transition::Transition, model::PetriNet)
-    for predecessor in get_predecessors(transition, model)
-        predecessor.marking -= 1
+function fire_sequence(model::AbstractPetriNet, sequence::Array{Int})
+    output_model = deepcopy(model)
+    for t in sequence
+        fire!(output_model, t)
     end
-    for successor in get_successors(transition, model)
-        successor.marking += 1
-    end
-    return model
+    return output_model
 end
 
-function add_place!(model::PetriNet, marking::Int)
+
+function is_enabled(model::AbstractPetriNet, transition_id::Int)
+    transition = model.transitions[transition_id]
+    return reduce(&, [(place.marking >= 1) for place in transition.from])
+end
+
+
+function add_place!(model::AbstractPetriNet, marking::Int)
     new_place_id = length(model.places) + 1
     push!(model.places, Place(new_place_id, marking))
     return model
 end
 
-function add_transition!(model::PetriNet)
-    new_transition_id = length(model.transitions) + 1
-    push!(model.transitions, Transition(new_transition_id))
+
+function add_transition!(model::AbstractPetriNet, from::Array{Place}, to::Array{Place})
+    push!(model.transitions, Transition(length(model.transitions) + 1, from, to))
     return model
 end
 
-function add_arc!(model::PetriNet, from::Place, to::Transition)
-    push!(model.arcs, (from, to))
+
+function add_transition!(model::AbstractPetriNet, from::Array{Int}, to::Array{Int})
+    predecessors = [p for p in model.places if p.id in from]
+    successors = [p for p in model.places if p.id in to]
+    push!(
+        model.transitions,
+        Transition(length(model.transitions) + 1, predecessors, successors)
+    )
+end
+
+
+function add_transition!(model::AbstractPetriNet, from::Int, to::Int)
+    predecessor = model.places[from]
+    successor = model.places[to]
+    push!(
+        model.transitions,
+        Transition(length(model.transitions) + 1, [predecessor], [successor])
+    )
     return model
 end
 
-function add_arc!(model::PetriNet, from::Transition, to::Place)
-    push!(model.arcs, (from, to))
-    return model
-end
-
-# mutable struct WorkFlowNet <: PetriNet
-#     source::Int
-#     sink::Int
-#     # places::Array{Place}
-#     # transitions::Array{Transition}
-#     # arcs::Array{Union{Tuple{Place, Transition}, Tuple{Transition, Place}}}
-# end
-
-# light weight version test
-# places = [Place(1, 2), Place(2, 0)]
-# transitions = [Transition(1)]
-# arcs = [(places[1], transitions[1]), (transitions[1], places[2])]
-# model = PetriNet(places, transitions, arcs)
-# fire(model.transitions[1], model)
-# model
-# add_place!(model, 3)
-# add_transition!(model)
-# add_arc!(model, model.places[1], model.transitions[2])
-# add_arc!(model, model.transitions[2], model.places[2])
-# print(model) 
 
 
 # ----- MORE IDEAS ----- #
