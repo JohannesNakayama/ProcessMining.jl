356991748f7b12af2bb27298a0b8ce475fd41cb4
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-08-03 11:20:54 +0200

    Added files for inductive miner and quality

---
+++ src/miners/inductive_miner.jl
@@ -0,0 +1,4 @@
+# recursive execution of the 3 following steps:
+# 1. build dfg
+# 2. find cut
+# 3. split log
---
+++ src/quality_metrics.jl
@@ -0,0 +1,13 @@
+module Quality
+
+function replay_fitness()
+    # TODO: write function
+    return 42
+end
+
+function precision()
+    # TODO: write function
+    return 42
+end
+
+end
